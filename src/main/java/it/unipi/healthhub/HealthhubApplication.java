54d4bcb3c178c432920cdcb6b572297283468622
paolo-asus <paol.palumbo@gmail.com>
2024-05-18 17:51:21 +0200

    Initial commit

---
+++ src/main/java/it/unipi/healthhub/HealthhubApplication.java
@@ -0,0 +1,20 @@
+package it.unipi.healthhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HealthhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthhubApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/it/unipi/healthhub/HealthhubApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.healthhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
