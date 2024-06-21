0e131b2dda69f4dc592db5e2343d622dfe986c86
paolo-asus <paol.palumbo@gmail.com>
2024-06-21 17:42:28 +0200

    Migliorata home temporanea.

--- src/main/java/it/unipi/healthhub/controller/IndexController.java
+++ src/main/java/it/unipi/healthhub/controller/IndexController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping({"/", "/index"})
     public String index(Model model) {
-        model.addAttribute("title", "HealthHub");
         return "index";
     }
 }
