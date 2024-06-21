c75359af3d2c883e3798b0e6f006c49d96e2a6f2
paolo-asus <paol.palumbo@gmail.com>
2024-06-21 17:18:52 +0200

    Aggiunta una pseudo homepage e controller associato.

---
+++ src/main/java/it/unipi/healthhub/controller/IndexController.java
@@ -0,0 +1,14 @@
+package it.unipi.healthhub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("title", "HealthHub");
+        return "index";
+    }
+}
